b39279175cfd174819df423dfaa44fe6c8df42a4
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-08-08 21:23:33 +0500

    Update stats2.jsx

--- web/frontend/components/stats2.jsx
+++ web/frontend/components/stats2.jsx
@@ -4,10 +4,16 @@ export default function Stats2() {
     <>
       <div class={styles.tablecontainer2}>
         <div class={styles.tablerow2}>
-          <div class={styles.tablecell}>Sales From automation</div>
+          <div class={styles.tablecell}>Sales automation</div>
           <div class={styles.tablecell}>Credits Left</div>
           <div class={styles.tablecell}>Time Saved</div>
           <div class={styles.tablecell}>Total Sales from Emails</div>
+        </div>
+        <div class={styles.tablerow2}>
+          <div class={styles.tablecell}>3601</div>
+          <div class={styles.tablecell}>24</div>
+          <div class={styles.tablecell}>150</div>
+          <div class={styles.tablecell}>PKR 0.00</div>
         </div>
       </div>
     </>
