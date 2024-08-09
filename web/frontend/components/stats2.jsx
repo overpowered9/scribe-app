e63e7062afde93d761bac681b007ae354e0b2e73
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-08-09 20:28:27 +0500

    Update stats2.jsx

--- web/frontend/components/stats2.jsx
+++ web/frontend/components/stats2.jsx
@@ -2,18 +2,18 @@ import styles from "../css/General.module.css";
 export default function Stats2() {
   return (
     <>
-      <div class={styles.tablecontainer2}>
-        <div class={styles.tablerow2}>
-          <div class={styles.tablecell}>Sales automation</div>
-          <div class={styles.tablecell}>Credits Left</div>
-          <div class={styles.tablecell}>Time Saved</div>
-          <div class={styles.tablecell}>Total Sales from Emails</div>
+      <div className={styles.tablecontainer2}>
+        <div className={styles.tablerow2}>
+          <div className={styles.tablecell}>Sales automation</div>
+          <div className={styles.tablecell}>Credits Left</div>
+          <div className={styles.tablecell}>Time Saved</div>
+          <div className={styles.tablecell}>Total Sales from Emails</div>
         </div>
-        <div class={styles.tablerow2}>
-          <div class={styles.tablecell}>3601</div>
-          <div class={styles.tablecell}>24</div>
-          <div class={styles.tablecell}>150</div>
-          <div class={styles.tablecell}>PKR 0.00</div>
+        <div className={styles.tablerow2}>
+          <div className={styles.tablecell}>3601</div>
+          <div className={styles.tablecell}>24</div>
+          <div className={styles.tablecell}>150</div>
+          <div className={styles.tablecell}>PKR 0.00</div>
         </div>
       </div>
     </>
